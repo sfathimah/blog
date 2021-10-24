c44b68ff88ded89b6cb578d8791b3b9694d4a78e
hanisahmohamad <hanisah10010@gmail.com>
2021-10-25 03:12:20 +0800

    aaa

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -40,7 +40,7 @@
         <i class="c-sidebar-nav-icon cil-medical-cross"></i>Manage Medical Record
 </a>
 <a class="c-sidebar-nav-link c-active" href="{{ route('pages.workload.appointmentSetting') }}">
-        <i class="c-sidebar-nav-icon cil-chart-pie"></i>Manage Workload Settings
+        <i class="c-sidebar-nav-icon cil-chart-pie"></i>Manage Workload Settings ini
 </a>
 </li></ul>
         </li>
